fccd4060a0aa701c577df12ce6f2dc276333e3be
Tomic92 <ticisternas@gmail.com>
2022-08-12 20:57:36 -0300

    terminada la DB

---
+++ api/src/models/Activity.js
@@ -0,0 +1,24 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('activity', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dificulty: {
+      type: DataTypes.ENUM('1','2','3','4','5'),
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    season: {
+      type: DataTypes.ENUM('summer', 'autumn', 'winter', 'spring'),
+      allowNull: false,
+    },
+  });
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define("country", {
+    id: {
+      type: DataTypes.CHAR(3),
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flag: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.STRING,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+    },
+    createdInDb:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
